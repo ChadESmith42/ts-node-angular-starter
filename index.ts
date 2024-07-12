0701fe718c49839936f4dd8c508afab9bc5b8c89
Chad Smith <chad.e.smith@outlook.com>
2024-07-12 17:50:31 -0500

    ADD basic server setup

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
-let message: string = "Hello World";
-console.log(message);
+import * as express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT: number = Number(process.env.PORT) || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+    console.info(`Server is listening on port ${PORT}`)
+});
